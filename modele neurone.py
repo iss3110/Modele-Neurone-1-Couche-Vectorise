df29e0efbf836ea81423ba17d11df09a623ab347
isslao <78855647+iss3110@users.noreply.github.com>
2022-05-16 10:54:47 +0200

    création du modèle du neurone, sur un data set simulé, entrainement du modèle, ajout d'une nouvelle entrée, prédiction, tracé l'évolution de log loss, et frontière de décision.

--- modele neurone.py
+++ modele neurone.py
@@ -1,20 +1,23 @@
-from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
 from utilities import *
 
-def artificial_neuron(X,y,learning_rate=0.1,n_iter=100):
-  #initialisation
-  W,b=initialisation(X)
-  Loss=[] #pour stocker les valeurs de coût à chaque itération, et ainsi suivre l'exécution de l'algo
-  for i in range(n_iter):
-    A=model(X,W,b)
-    Loss.append(log_loss(A,y))
-    dW,db=gradients(A,X,y)
-    W,b=update(dW,db,W,b,learning_rate)
-  
-  y_pred=predict(X,W,b) # prédiction pour toutes les données du modèle
-  print(accuracy_score(y,y_pred))
+X, y = make_blobs(n_samples=100, n_features=2, centers=2, random_state=0)
+y = y.reshape((y.shape[0],1))
 
-  plt.plot(Loss)
-  plt.show()
-  return(W,b)
+print('dimensions de X:', X.shape)
+print('dimensions de y:', y.shape)
+
+W,b=artificial_neuron(X,y)
+new_plant = np.array([2,1])
+
+predict(new_plant,W,b)
+# La frontière de décision corréspond à A=1/2 ou bien Z=0,
+# cad w1.x1 + w2.x2 + b = 0
+# ou x2 = -(w1.x1 + b)/w2
+fig, ax = plt.subplots(figsize=(9, 6))
+ax.scatter(X[:,0], X[:, 1], c=y, cmap='summer')
+x1=np.linspace(-1,4,100) # un vecteur entre -1 et 4 avec 100 valeurs
+x2=-(W[0]*x1+b)/W[1]
+plt.plot(x1,x2,c="blue",lw=3)
+plt.scatter(new_plant[0], new_plant[1],c="r")
+plt.show() # remarque que cette ligne qui gère l'affichage du plot, donc permet d'afficher différents plot sur le même graphe ou non
